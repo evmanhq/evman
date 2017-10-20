d4f4a1d004b4e29e7c8a321336d9f50a8fb4a7c3
Marek Jelen <marek@jelen.biz>
2017-10-20 08:29:00 +0200

    Better concurrency (hopefully)

--- docker/evman.sh
+++ docker/evman.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 bundle exec rake db:migrate
-bundle exec puma -e $RAILS_ENV -b tcp://127.0.0.1:3000 --state=/home/evman/evman.state
+bundle exec puma -w 3 -t 5:5 -e $RAILS_ENV -b tcp://127.0.0.1:3000 --state=/home/evman/evman.state
