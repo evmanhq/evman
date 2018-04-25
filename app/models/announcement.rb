635e136c31b6664ed023308fa9462544eecd5671
Filip Zachar <tulak45@gmail.com>
2018-04-25 03:59:03 +0200

    Implements Announcement.new_for_user as a scope
    For better usage with ActiveRecord::Relation

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -3,11 +3,11 @@ class Announcement < ApplicationRecord
   has_many :announcement_users, inverse_of: :announcement, dependent: :destroy
   belongs_to :team
 
-  def self.new_for_user(user)
+  scope :new_for_user, -> (user) {
     Announcement
-      .where(team: [nil] + user.teams)
-      .where.not(id: AnnouncementUser.select(:announcement_id).where(user: user))
-      .order(id: :desc)
-  end
+        .where(team: [nil] + user.teams)
+        .where.not(id: AnnouncementUser.select(:announcement_id).where(user: user))
+        .order(id: :desc)
+  }
 
 end
