90f3aaf0254a3bd2fa21c7bbd41468316f719e3c
Filip Zachar <tulak45@gmail.com>
2018-07-25 13:20:10 +0200

    Shared Events - fix event properties when editing
    Load event properties from the event’s team instead of the current_team since it may be shared from another team which has different event_properties

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,6 @@
 module EventsHelper
-  def event_properties_form_definition
-    event_properties = current_team.event_properties.in_order.includes(:options)
+  def event_properties_form_definition(event)
+    event_properties = event.team.event_properties.in_order.includes(:options)
 
     event_properties.as_json(only: [:id, :name, :restrictions, :behaviour, :required],
                              include: {
